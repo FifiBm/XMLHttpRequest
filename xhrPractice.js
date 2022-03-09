4724739a1ca65377ecea269c2c2495e289189937
FifiBm <fifibm90@gmail.com>
2022-03-08 17:09:17 -0800

    this is xhrpractice

--- xhrPractice.js
+++ xhrPractice.js
@@ -8,11 +8,17 @@
 //create a variable called xhr and set it equal to a new XMLHttpRequest()
 
 //create a variable for url and set it equal to 'https://dog.ceo/api/breeds/list/all'
+var XMLHttpRequest = require ( "xhr2" );
+var xhr = new XMLHttpRequest();
 
-var method = 'GET'; 
+var url = 'https://dog.ceo/api/breeds/list/all';
+
+var method = 'GET';
 
 //use xhr variable with open method
 //pass in methos and url as paramenters
+
+xhr.open (method, url);
 
 
 //when we make to connection, this is what the property will be
@@ -23,6 +29,24 @@ var method = 'GET';
 //create parsedJSON varible for JSON.parse method
 //log beagle, elkhound, hound, papillon, and spaniel
 //use else/if to log error
+
+xhr.open (method, url);
+xhr.onreadystatechange = function(){
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+        var parsedJSON = JSON.parse(xhr.responseText).message
+        //console.log(parsedJSON);
+        console.log(parsedJSON.beagle);
+        console.log(parsedJSON.elkhound);
+        console.log(parsedJSON.hound);
+        console.log(parsedJSON.papillon);
+        console.log(parsedJSON.spaniel);
+    } else if(xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200){
+    console.log('Error')}
+        else {
+            console.log('Error')
+        }
+    }
+
 
 
 //use send 
